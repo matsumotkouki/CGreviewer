cb66a55dd3e4f7733e79e5058978fb67609f3240
matsumotokouki <18matsumoto@nao.net.it-chiba.ac.jp>
2018-06-19 20:19:35 +0900

    first commit

---
+++ CGreview_sakura/the_work_load.js
@@ -0,0 +1,35 @@
+
+//作品表示？
+function list_create(model,img,name){
+    var id = document.getElementById("list_target");
+    var div1 = document.createElement("div");
+    var img1 = document.createElement("img"); 
+    var a1 = document.createElement("a");
+    var b1 = document.createElement("a");
+    var list_model = model;
+    var list_user = name;
+    img1.src='./assets/work_files/'+img;
+    img1.style.width="300px";
+    img1.style.height="200px";
+    img1.style.border="medium double black"
+    //aタグにリンクを追加
+    a1.href = 'index.php?sample1='+list_model;
+    //文字表示
+    a1.innerHTML = list_model;
+    b1.href = 'user_data.php?sample1='+list_user;
+    b1.innerHTML = list_user;
+
+    id.appendChild(div1); 
+    //サムネイル表示
+    div1.appendChild(img1); 
+    //作品名表示
+    div1.appendChild(a1);
+    div1.appendChild(b1); 
+    console.log(div1);
+    console.log(img1);
+    console.log(a1);
+    console.log(b1);
+}
+function thumbnail_create(thumbnail){
+    console.log(thumbnail);
+}
